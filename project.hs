48c98c2d8622f62ca5d405ea30ad71e4b49855be
Rita Lopes <up202108819@up.pt>
2023-12-30 15:09:20 +0000

    Started top level parse function

--- project.hs
+++ project.hs
@@ -1,10 +1,10 @@
 module Project where
 
-import Data.Char
-import Data.Either
+import Data.Char ( isAlpha, isDigit, isSpace )
+import Data.Either ()
 import Data.Function (on)
 import Data.List (sortBy)
-import Debug.Trace
+import Debug.Trace ( trace )
 
 -- PFL 2023/24 - Haskell practical assignment quickstart
 -- Updated on 27/12/2023
@@ -237,8 +237,8 @@ Parsing Order (Reverse of priority)
  - Boolean Expressions: And -> Boolean Equality -> Not -> Integer Equality/Integer Inequality
 -}
 
-{- parse :: String -> Program
-parse = undefined -- TODO -}
+parse :: String -> Program
+parse = parse 
 
 {- parse :: [Token] -> Program
 parse tokens =
@@ -438,3 +438,8 @@ testParser programCode = (stack2Str stack, state2Str state)
 -- testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
 -- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
 -- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+
+{- 
+Regras assumidas sobre a linguagem:
+
+- -}
